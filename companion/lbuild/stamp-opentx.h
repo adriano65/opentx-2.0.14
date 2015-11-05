0f69679200af37dc3db08d22adf620ce2c855dfc
adriano65 <adriano.carosso@gmail.com>
2015-11-05 15:59:55 +0100

    Some CMakeFile enhancements

--- companion/lbuild/stamp-opentx.h
+++ companion/lbuild/stamp-opentx.h
@@ -1,4 +1,4 @@
 // Automatically generated file (CMake) - do not edit
-#define DATE_STR  "04.11.2015"
-#define TIME_STR  "21:44:22"
+#define DATE_STR  "05.11.2015"
+#define TIME_STR  "15:49:14"
 #define VERS_STR  "2.0.14a"
